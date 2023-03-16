bb3c0cf1b98c22978f2f56a1755abb2bdb9d1a0f
Einar Norðfjörð <einar@nordfjord.io>
2023-03-15 23:56:02 -0400

    repro

---
+++ Program.fs
@@ -0,0 +1,44 @@
+﻿module Program
+
+open Propulsion.Internal
+open Serilog
+open System
+
+[<EntryPoint>]
+let main _ =
+
+  let log: ILogger = LoggerConfiguration().WriteTo.Console().CreateLogger()
+
+  let stats =
+    { new Propulsion.Streams.Stats<_>(log, TimeSpan.FromMinutes 1, TimeSpan.FromMinutes 1) with
+        member _.HandleOk x = ()
+        member _.HandleExn(log, x) = () }
+
+  let checkpoints =
+    Propulsion.MessageDb.ReaderCheckpoint.CheckpointStore(
+      "Host=localhost; Port=5432; Username=postgres; Password=postgres; Database=message_store",
+      "message_store",
+      "LocalTest2",
+      (TimeSpan.FromSeconds 5)
+    )
+    
+  checkpoints.CreateSchemaIfNotExists() |> Async.AwaitTask |> Async.RunSynchronously
+
+  let handle (stream: FsCodec.StreamName) (events: Propulsion.Streams.Default.StreamSpan) _ct =
+    task {
+      log.Information("Handling {stream_name}", stream)
+      return struct (Propulsion.Streams.SpanResult.AllProcessed, ())
+    }
+
+  use sink =
+    Propulsion.Streams.Default.Config.Start(log, 100, 1, handle, stats, TimeSpan.FromMinutes 1)
+
+  use client = EventStore.Client.EventStoreClient(EventStore.Client.EventStoreClientSettings.Create("esdb://localhost:2113?tls=false"))
+
+  let source =
+    Propulsion.EventStoreDb.EventStoreSource(log, TimeSpan.FromMinutes 1, client, 500L, TimeSpan.FromMilliseconds 200, checkpoints, sink)
+
+  use src = source.Start()
+  src.AwaitWithStopOnCancellation() |> Async.RunSynchronously
+  0
+
---
+++ readme.md
@@ -0,0 +1,7 @@
+to run
+
+```
+docker-compose up -d
+# wait around 10 secs for DB to get warm
+dotnet run
+```
